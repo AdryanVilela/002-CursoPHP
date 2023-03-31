f432fd78ca881e94d2100d6dbfb55dd2cc070194
Adryan Vilela <122546032+AdryanVilela@users.noreply.github.com>
2023-03-31 15:14:12 -0300

    Modulo de Variaveis

---
+++ Variaveis/basico/comentarios.php
@@ -0,0 +1,20 @@
+<div class="titulo">Comentarios PHP</div>
+<?php 
+echo "estou no PHP";
+//echo "estou no PHP";
+#echo "estou no PHP";
+// echo # "Comentario";
+# ?>
+<p>Depois Do primeiro Bloco!</p>
+
+<!--<p>Comentario HTML</p>-->
+
+<?php 
+/*  LINHA 1 DO COmentario
+    LINHA 1 DO COmentario
+    LINHA 1 DO COmentario
+*/
+?>
+<p>Depois do segundo bloco!</p>
+
+
